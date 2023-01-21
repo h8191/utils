fb1d72c7ce9b259980366101796fe46c80474782
h8191 <45847870+h8191@users.noreply.github.com>
2023-01-21 17:29:33 +0530

    Add files via upload

---
+++ neovim/init_nvim.sh
@@ -0,0 +1,18 @@
+#https://www.youtube.com/watch?v=JWReY93Vl6g
+
+
+mkdir .config/nvim/ -p 
+cd .config/nvim
+touch init.vim
+#nvim ~/.config/nvim/init.vim
+
+
+#https://github.com/NeuralNine/config-files/blob/master/init.vim
+#https://github.com/junegunn/vim-plug
+############curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#sudo apt install git #if you don't already have git 
+#sudo apt install exuberant-ctags
+
+
